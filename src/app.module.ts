448b120fd7145124493cc6c6df2d353d33530cbc
mahmoudOdeh99 <mahmoud.odeh@sitech.me>
2025-03-06 01:51:10 +0300

    integration with i18n to suppourt multi lang

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,24 @@ import { DatabaseModule } from './database/database.module';
 import { UserModule } from './users/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
+import { AcceptLanguageResolver, HeaderResolver, I18nModule, QueryResolver } from 'nestjs-i18n';
+import * as path from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      loaderOptions: {
+        path: path.join(__dirname, '/i18n/'),
+        watch: true,
+      },
+      resolvers: [
+        { use: QueryResolver, options: ['lang'] },
+        AcceptLanguageResolver,
+        new HeaderResolver(['x-lang']),
+      ],
+    }),
     DatabaseModule,
     UserModule,
     CloudinaryModule,
